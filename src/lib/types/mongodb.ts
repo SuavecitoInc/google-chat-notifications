f848b7ef2d00251dd6f889bcedc4b5dc2568be23
j-Riv <jose.alfredo.rivera@gmail.com>
2025-04-24 12:51:39 -0700

    chore: Added MongoDB Webhook Payload type

---
+++ src/lib/types/mongodb.ts
@@ -0,0 +1,8 @@
+export type MongoDBPayload = {
+  created: string; // '2024-09-24T23:13:43.120Z';
+  id: string;
+  humanReadable: string;
+  updated: string;
+  orgId: string;
+  status: string;
+};
